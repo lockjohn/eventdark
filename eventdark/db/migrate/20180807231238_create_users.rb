ea9e8ab627857f8cfcb4ded999285afe24e33938
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-08-07 16:26:18 -0700

    Migrate User table

---
+++ eventdark/db/migrate/20180807231238_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null:false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.boolean :organizer, default: false
+      t.string :organizer_description, null:true
+
+      t.timestamps
+    end
+  end
+end
