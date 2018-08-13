2962949a52c73fffdc99a02f6db7eb5c54fd2e24
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-08-13 13:29:09 -0700

    Add User has_many association for events see if fixes heroku bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
     
     after_initialize :ensure_session_token
 
+    has_many :events, class_name: :Event, foreign_key: "organizer_id"
+
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         return nil unless user
