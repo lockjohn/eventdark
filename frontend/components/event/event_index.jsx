a84038bb92f80d0b1742d3d10ab12744fc874f64
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-08-16 15:24:15 -0700

    Finish category index css layout

--- frontend/components/event/event_index.jsx
+++ frontend/components/event/event_index.jsx
@@ -11,15 +11,7 @@ render () {
     return (
         <div className="ei-grid">
             {events.map(event=> <EventIndexItem key={event.id} event={event}/>)}
-            <div className="ei-cat-img-grid">
-                <div className="cat-img-1"><img src="" alt=""/> Rugged</div>
-                <div className="cat-img-2"><img src="" alt=""/> Aesthetic</div>
-                <div className="cat-img-3"><img src="" alt=""/> Violent</div>
-                <div className="cat-img-4"><img src="" alt=""/> Culinary</div>
-                <div className="cat-img-5"><img src="" alt=""/> Linguistic</div>
-                <div className="cat-img-6"><img src="" alt=""/> Urbane</div>
-                <div className="cat-img-7"><img src="" alt=""/> Equestrian</div>
-            </div>
+          
         </div>
 
     );
--- frontend/components/home/home_component.jsx
+++ frontend/components/home/home_component.jsx
@@ -27,8 +27,16 @@ class Home extends React.Component {
                     </div>
                         <header className="event-index-header">Events for you</header>
                         <EventIndex events={this.props.entities.events} />
-                    <header className="event-index-header">Militaristic</header>
-                    <EventIndex events={this.props.entities.events} />
+                    <header className="event-index-header">Browse by Top Categories</header>
+                    <div className="ei-cat-img-grid">
+                        <div className="cat-img-1"><img src="" alt="" /> <p>Rugged</p></div>
+                        <div className="cat-img-2"><img src="" alt="" /><p> Aesthetic</p></div>
+                        <div className="cat-img-3"><img src="" alt="" /><p> Violent</p></div>
+                        <div className="cat-img-4"><img src="" alt="" /><p> Culinary</p></div>
+                        <div className="cat-img-5"><img src="" alt="" /><p> Linguistic</p></div>
+                        {/* <div className="cat-img-6"><img src="" alt="" /> Urbane</div>
+                        <div className="cat-img-7"><img src="" alt="" /> Equestrian</div> */}
+                    </div>
                 </div>
             </div>
         )
