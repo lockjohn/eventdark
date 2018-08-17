137d0e3b2248d5bf391b5bf5383816b6fb9b9156
John Lockhart <lockjohn@Johns-MacBook-Air.local>
2018-08-17 14:34:53 -0700

    revert to original webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,29 @@
-var path = require("path");
-var webpack = require("webpack");
+const path = require('path');
 
-// var plugins = []; // if using any plugins for both dev and production
-// var devPlugins = []; // if using any plugins for development
-
-// var prodPlugins = [
-//     new webpack.DefinePlugin({
-//         'process.env': {
-//             'NODE_ENV': JSON.stringify('production')
-//         }
-//     }),
-//     new webpack.optimize.UglifyJsPlugin({
-//         compress: {
-//             warnings: true
-//         }
-//     })
-// ];
-
-// plugins = plugins.concat(
-//     process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// )
-
-// include plugins config
 module.exports = {
     context: __dirname,
-    entry: "./frontend/event_dark.jsx",
+    entry: './frontend/event_dark.jsx',
     output: {
-        path: path.resolve(__dirname, "app", "assets", "javascripts"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
     },
-    // plugins: plugins, 
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'env']
-                }
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['env', 'react']
+                    }
+                },
             }
         ]
     },
-    devtool: 'source-map',
-    resolve: {
-        extensions: [".js", ".jsx", "*"]
-    }
+    devtool: 'source-map'
 };
+
